a71c0fb5b5d9b13951c939bc305e31cea9fa0fff
Nham Mai <nhammaikhanh@gmail.com>
2023-11-13 16:20:08 +0700

    change play to use button and style it more beutiful

--- script.js
+++ script.js
@@ -24,14 +24,54 @@ function playRound(playerSelection, computerSelection) {
 }
 
 
+let playerScore = 0;
+let computerScore = 0;
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt("Enter your choice (Rock, Paper, or Scissors):");
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
+const rockButton = document.getElementById('rock');
+const paperButton = document.getElementById('paper');
+const scissorsButton = document.getElementById('scissors');
+const playAgainButton = document.getElementById('playAgain');
+
+rockButton.addEventListener('click', function() {
+    playGame('Rock');
+});
+paperButton.addEventListener('click', function() {
+    playGame('Paper');
+});
+scissorsButton.addEventListener('click', function() {
+    playGame('Scissors');
+});
+
+
+playAgainButton.addEventListener('click', function() {
+    playerScore = 0;
+    computerScore = 0;
+    document.getElementById('score').textContent = '';
+    document.getElementById('result').textContent = '';
+    rockButton.disabled = false;
+    paperButton.disabled = false;
+    scissorsButton.disabled = false;
+    playAgainButton.classList.add('hidden');
+});
+
+
+function playGame(playerSelection) {
+    const computerSelection = getComputerChoice();
+    const result = playRound(playerSelection, computerSelection);
+    document.getElementById('result').textContent = result;
+    if (result.includes('Win')) {
+        playerScore++;
+    } else if (result.includes('Lose')) {
+        computerScore++;
+    }
+    document.getElementById('score').textContent = `Player: ${playerScore}, Computer: ${computerScore}`;
+    if (playerScore === 5 || computerScore === 5) {
+        const winner = playerScore === 5 ? 'Player' : 'Computer';
+        document.getElementById('result').textContent = `${winner} wins the game!`;
+        rockButton.disabled = true;
+        paperButton.disabled = true;
+        scissorsButton.disabled = true;
+        playAgainButton.classList.remove('hidden');
     }
 }
-
-game();
-
+       
